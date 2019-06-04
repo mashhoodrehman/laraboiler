2a719c15d2733a796546f49df20f2cdb66428ee4
mashhood <mashhoodr.rehman@gmail.com>
2019-06-04 15:17:17 +0500

    login message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,6 +27,7 @@ class AuthController extends Controller
     {
         $credentials = request(['email', 'password']);
         if (! $token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized' , 'code' => 401]);
             return response()->json(['error' => 'Unauthorized'], 401);
         }
         return $token;
